398d6e847cee69031b0cd650930c7f8cf4b8ada0
deissypc <deissyparrac@gmail.com>
2022-08-30 21:20:04 -0500

    Se ha generado el index y el primer ejercicio con estilo

--- js/ejercicio1.js
+++ js/ejercicio1.js
@@ -1,7 +1,7 @@
 /*longitud palabra*/
 function lon(){
     const lon =document.getElementById('p').value.length;
-    alert("la longitud que tiene la poalabra es: "+lon)
+    alert("la longitud que tiene la palabra es: " +lon)
     }
     /*palabra en mayuscula*/
 function mayus(){
@@ -16,6 +16,6 @@ let minus=document.getElementById('p').value;
  /*primer caracter*/
 function primero(){
 let primero =document.getElementById('p').value[0];
-                alert ("el primer caracter de la palbra es:"  +primero.toUpperCase());
+                alert ("el primer caracter de la palabra es: "  +primero.toUpperCase());
 
                 }
